73c74a2ddcc6a58be40e3ecaacc02f59534b1119
sichoi42 <42.4.sichoi@gmail.com>
2022-06-15 23:33:20 +0900

    :coffin: remove unused variable
    remove unused variable

--- trace/ray/texture.c
+++ trace/ray/texture.c
@@ -6,7 +6,7 @@
 /*   By: sichoi <sichoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 15:42:46 by sichoi            #+#    #+#             */
-/*   Updated: 2022/06/15 17:06:03 by sichoi           ###   ########.fr       */
+/*   Updated: 2022/06/15 23:31:09 by sichoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 void	checkerboard(t_hit_record *rec)
 {
-	int			black;
-	int			white;
 	t_point3	is_black;
 
-	black = BLACK;
-	white = WHITE;
 	is_black.x = abs((int)floor(rec->p.x)) & 1;
 	is_black.y = abs((int)floor(rec->p.y)) & 1;
 	is_black.z = abs((int)floor(rec->p.z)) & 1;
